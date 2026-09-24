35e7dc1025c293996ea45f51906bc94b0ece1b9c
Alejandro <alejodev2022@gmail.com>
2026-09-23 19:44:58 -0500

    Add language support by creating index.ts to manage translations for English and Spanish, including a function to retrieve messages based on selected language.

---
+++ src/app/language/index.ts
@@ -0,0 +1,10 @@
+import en from "./en.json";
+import es from "./es.json";
+
+export const languages = { es, en } as const;
+export type Language = keyof typeof languages;
+export type Messages = typeof es;
+
+export function getMessages(language: Language = "es"): Messages {
+  return languages[language] as Messages;
+}
